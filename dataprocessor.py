ce6493e40b63ea1bdd5ef6c3c49ecb904f7aa6aa
Kaylee Pont <kayleepont@re-byods-145-3-246-13.wireless.hva.nl>
2023-04-21 16:55:52 +0200

    initual commit

---
+++ dataprocessor.py
@@ -0,0 +1,7 @@
+# Code voor vergelijken van daadwerkelijke versnelling samen met geregistreerde versnelling. 
+# openen van data file
+import pandas as pd
+import numpy as np
+import csv
+csvfile = pd.read_csv('/Users/kayleepont/Desktop/Files/School/Technische natuurkunde HVA/Project 1.4/Visualcode/posities_1_Team_A2.csv')
+print(csvfile)
