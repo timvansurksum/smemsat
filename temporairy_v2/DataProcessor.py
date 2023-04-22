d6a4aa424b3531cc25c7327f8b02e208cf81113f
Tim <targa2002260@gmail.com>
2023-04-22 15:42:12 +0200

    added a version with classes in a folder calles temporairy_v2

---
+++ temporairy_v2/DataProcessor.py
@@ -0,0 +1,36 @@
+# hier worden de gebruikte libraries geimporteerd. 
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+
+class DataProcessor():
+    def load_Data(file):
+        # deze lijn zorgt ervoor dat de data file wordt gelezen. 
+        
+        data = np.genfromtxt(file,delimiter='   ')
+        return data
+
+    def differentiate(x, t):
+        # hier wordt er een lege lijst gemaakt voor v, vervolgens wordt x (positie) gedifferentieerd om v (snelheid) te krijgen. 
+        v = [] 
+        for i in range(0,len(t)-1):
+            dx= x[i+1]-x[i]
+            dt= t[i+1]-t[i]
+            afgeleide = dx/dt
+            v.append(afgeleide)
+        return v, t[:-1]
+    
+    @classmethod
+    def Plot_Data(self, t, a_1, a_2):
+        # het maken van een array voor beide versnellingsprofielen. de t staat zelf al wel in een array dus die hoefde niet veranderd te worden. 
+        a_array= np.array(a_1)
+        a_twee_array = np.array(a_2)
+
+        # beide datasets hebben nu een versnellingsprofiel die in een array staan. Deze kunnen nu samen in een grafiek gezet worden. 
+        plt.plot(t,a_array, label= 'versnelling_dataset1' )
+        plt.plot(t,a_twee_array, label= 'versnelling_dataset2')
+        plt.xlabel('tijd[s]')
+        plt.ylabel('Versnelling[m/sˆ2]')
+        plt.title("Versnellingsprofielen van twee datasets")
+        plt.legend()
+        plt.show()
---
+++ temporairy_v2/startup.py
@@ -0,0 +1,28 @@
+from DataProcessor import DataProcessor
+
+# de file paden van de datafiles
+file_1 = 'posities_1_Team_A2.txt'
+file_2 = 'posities_2_Team_A2.txt'
+
+# deze funcie laad de data van de data bestanden in met de hierboven gegeven paden
+data_1 = DataProcessor.load_Data(file_1)
+data_2 = DataProcessor.load_Data(file_2)
+
+# deze regels geven een concrete definitie voor x en t wat het dus makkelijker maakt om versnellingsprofielen te maken. 
+t_1 = data_1[:,0]
+x_1 = data_1[:,1]
+
+t_2 = data_2[:,0]
+x_2 = data_2[:,1]
+
+#deze functie leid de plaats informatie af en geeft snelheid
+v_1, t_1 = DataProcessor.differentiate(x_1, t_1)
+v_2, t_2 = DataProcessor.differentiate(x_2, t_2)
+
+
+#deze functie leid de snelheids informatie af en geeft versnelling
+a_1, t_1 = DataProcessor.differentiate(v_1, t_1)
+a_2, t_2 = DataProcessor.differentiate(v_2, t_2)
+
+#deze functie plot de data van beide bronnen
+DataProcessor.Plot_Data(t_1, a_1,a_2)
