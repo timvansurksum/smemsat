33ddf9baeb5fc425ea1c64e16d93d4c59e665c71
Tim <targa2002260@gmail.com>
2023-05-10 08:52:27 +0200

    cleaned code & added analysis for ideal and non ideal accelerometers

--- temporairy_v2/DataProcessor.py
+++ temporairy_v2/DataProcessor.py
@@ -4,14 +4,138 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class DataProcessor():
-    def load_Data(file):
-        # deze lijn zorgt ervoor dat de data file wordt gelezen. 
+    def get_full_response_data(
+        acceleratie_frame: list, 
+        timestamps_frame: list, 
+        response_bij_t_0: float, 
+        response_bij_t_0_plus_dt: float, 
+        dempingsfactor: float, 
+        massa: float, 
+        veerconstante: float
+        ):
+        """
+        Get_full_response_data
+        ----------
+        Deze functie gebruikt de parameters van de accelerometer
+        en het acceleratie profiel van het frame om numeriek
+        de response van de massa te bepalen.
+
+        Parameters
+        ----------
+        - acceleratie_frame : list, 
+            de versnelling van het frame
+        - timestamps_frame : list, 
+            Tijdsmomenten van data.
+        - dempingsfactor : float, 
+            de dempingsfactor gamma is de denpingsfactor van het massaveersysteem.
+        - response_bij_t_0 : float,
+            de response op het startmoment van het versnellingsprofiel.
+        - response_bij_t_0_plus_dt: float, 
+            De response een tijdsmoment dt na de start van het versnellingsprofiel.
+        - massa : float, 
+            De de massa van de massa
+        - veerconstante : float, 
+            het collectieve veerconstante van alle veren in het massa veersysteem
         
-        data = np.genfromtxt(file,delimiter='   ')
+        Returns
+        ----------
+        response_acceleration_data : list
+            -   een reponse profiel op het gegeven acceleratie profiel in de vorm van een lijst.
+        """
+        
+        input_data_indexes = range(1, len(acceleratie_frame))
+        response_acceleration_data = [response_bij_t_0, response_bij_t_0_plus_dt]
+        
+        for input_data_index in input_data_indexes:
+            response_at_t = DataProcessor.response_T_Plus_dt(
+                dempingsfactor = dempingsfactor,
+                massa = massa,
+                veerconstante = veerconstante,
+                acceleratie_frame_t = acceleratie_frame[input_data_index],
+                response_t = response_acceleration_data[input_data_index],
+                response_t_min_dt = response_acceleration_data[input_data_index - 1],
+                dt = timestamps_frame[input_data_index] - timestamps_frame[input_data_index - 1]
+                )
+            response_acceleration_data.append(response_at_t)
+        return response_acceleration_data
+        
+    def response_T_Plus_dt(
+        dempingsfactor: float, 
+        massa: float, 
+        veerconstante: float, 
+        acceleratie_frame_t: float, 
+        response_t: float, 
+        response_t_min_dt: float, 
+        dt: float
+        ):
+        """
+        Response_T_Plus_dt
+        ----------
+        Berekend de response in een gegeven punt
+        
+        Parameters
+        ----------
+        - dempingsfactor: float, 
+            de dempingsfactor gamma is de denpingsfactor van het massaveersysteem.
+        - massa: float, 
+            De de massa van de massa
+        - veerconstante: float, 
+            het collectieve veerconstante van alle veren in het massa veersysteem
+        - acceleratie_frame_t: float, 
+            de versnelling van het frame op een gegeven tijd t
+        - response_t: 
+            float, de response of relatieve positie van de massa ten opzichten van het frame op een gegeven tijd t
+        - response_t_min_dt: float, 
+            de response of relatieve positie van de massa ten opzichten van het frame op een gegeven tijd t - h
+        - dt: float, 
+            Het verschil in tijdsmomenten.
+            
+        Returns
+        ----------
+        - response_t_plus_dt : float
+            de response van een gegeven punt
+        
+        """
+        response_t_plus_dt = (
+                              response_t            * ( 2 - (dempingsfactor*dt/massa) - (dt**2)*veerconstante/massa )
+                            - response_t_min_dt     * ( 1 - (dempingsfactor*dt/massa) ) 
+                            + acceleratie_frame_t   * ( (dt**2) )
+                            )
+        return response_t_plus_dt
+
+    def load_Data(filepath: str):
+        """
+        Load_Data
+        ----------
+        laad de databestanden in op basis van een gegeven pad
+        
+        Parameters
+        ----------
+        - filepath: str
+            het pad van het bestand dat je wil inlezen
+        
+        Returns
+        ----------
+        - data: np.array
+            de data van het bestand in de vorm van een numpy array
+        """
+        
+        data = np.genfromtxt(filepath,delimiter='   ')
         return data
 
-    def differentiate(x, t):
-        # hier wordt er een lege lijst gemaakt voor v, vervolgens wordt x (positie) gedifferentieerd om v (snelheid) te krijgen. 
+    def differentiatie(x: list, t: list):
+        """
+        Differentiatie
+        --------------
+        hier wordt er een lege lijst gemaakt voor v, vervolgens wordt x (positie) gedifferentieerd om v (snelheid) te krijgen. 
+        
+        Parameters
+        ----------
+        - x: list
+            functie waardes waar over wordt gediferentieerd
+        - t: list
+            
+        """
         v = [] 
         for i in range(0,len(t)-1):
             dx= x[i+1]-x[i]
@@ -20,17 +144,35 @@ class DataProcessor():
             v.append(afgeleide)
         return v, t[:-1]
     
-    @classmethod
-    def plot_Data(self, t, a_1, a_2):
-        # het maken van een array voor beide versnellingsprofielen. de t staat zelf al wel in een array dus die hoefde niet veranderd te worden. 
-        a_array= np.array(a_1)
-        a_twee_array = np.array(a_2)
-
-        # beide datasets hebben nu een versnellingsprofiel die in een array staan. Deze kunnen nu samen in een grafiek gezet worden. 
-        plt.plot(t,a_array, label= 'versnelling_dataset1' )
-        plt.plot(t,a_twee_array, label= 'versnelling_dataset2')
-        plt.xlabel('tijd[s]')
-        plt.ylabel('Versnelling[m/sˆ2]')
-        plt.title("Versnellingsprofielen van twee datasets")
+    def plot_Data(
+        t: list, 
+        plot_title: str, 
+        x_axis_title: str, 
+        y_axis_title: str, 
+        dataset_1: list, 
+        graph_1_title: str, 
+        dataset_2: list, 
+        graph_2_title:str
+        ):
+        """
+        This function 
+        Parameters
+        ----------
+            - t: list, 
+            - plot_title: str, 
+            - x_axis_title: str, 
+            - y_axis_title: str, 
+            - dataset_1: list, 
+            - graph_1_title: str, 
+            - dataset_2: list, 
+            - graph_2_title:str
+        """
+        dataset_1_array= np.array(dataset_1)
+        dataset_2_array = np.array(dataset_2)
+        plt.plot(t,dataset_1_array, label=  graph_1_title)
+        plt.plot(t,dataset_2_array, label= graph_2_title)
+        plt.xlabel(x_axis_title)
+        plt.ylabel(y_axis_title)
+        plt.title(plot_title)
         plt.legend()
         plt.show()
--- temporairy_v2/startup.py
+++ temporairy_v2/startup.py
@@ -1,28 +1,113 @@
 from DataProcessor import DataProcessor
 
 # de file paden van de datafiles
-file_1 = 'posities_1_Team_A2.txt'
-file_2 = 'posities_2_Team_A2.txt'
+datafile_frame_1_pad = "posities_1_Team_A2.txt"
+datafile_frame_2_pad = "posities_2_Team_A2.txt"
 
 # deze funcie laad de data van de data bestanden in met de hierboven gegeven paden
-data_1 = DataProcessor.load_Data(file_1)
-data_2 = DataProcessor.load_Data(file_2)
+data_frame_1 = DataProcessor.load_Data(datafile_frame_1_pad)
+data_frame_2 = DataProcessor.load_Data(datafile_frame_2_pad)
 
 # deze regels geven een concrete definitie voor x en t wat het dus makkelijker maakt om versnellingsprofielen te maken. 
-t_1 = data_1[:,0]
-x_1 = data_1[:,1]
+timestamps_data_1 = data_frame_1[:,0]
+positie_data_frame_1 = data_frame_1[:,1]
 
-t_2 = data_2[:,0]
-x_2 = data_2[:,1]
+timestamps_data_2 = data_frame_2[:,0]
+positie_data_frame_2 = data_frame_2[:,1]
 
 #deze functie leid de plaats informatie af en geeft snelheid
-v_1, t_1 = DataProcessor.differentiate(x_1, t_1)
-v_2, t_2 = DataProcessor.differentiate(x_2, t_2)
-
+snelheid_data_frame_1, timestamps_data_1 = DataProcessor.differentiatie(positie_data_frame_1, timestamps_data_1)
+snelheid_data_frame_2, timestamps_data_2 = DataProcessor.differentiatie(positie_data_frame_2, timestamps_data_2)
 
 #deze functie leid de snelheids informatie af en geeft versnelling
-a_1, t_1 = DataProcessor.differentiate(v_1, t_1)
-a_2, t_2 = DataProcessor.differentiate(v_2, t_2)
+acceleratie_data_frame_1, timestamps_data_1 = DataProcessor.differentiatie(snelheid_data_frame_1, timestamps_data_1)
+acceleratie_data_frame_2, timestamps_data_2 = DataProcessor.differentiatie(snelheid_data_frame_2, timestamps_data_2)
 
-#deze functie plot de data van beide bronnen
-DataProcessor.plot_Data(t_1, a_1,a_2)
+
+response_bij_t_0 = 0
+response_bij_t_0_plus_dt = 0    # in theorie is kan dit ook niet nul zijn 
+        	                # maar gezien de stapgrootte heel klein is en beide datasets 
+                                # vrijwel beginnen bij 0 zal dit vrijwel geen invloed op het resultaat hebben
+
+# gegeven parameters
+massa = 1.476*10**-6
+dempingsfactor =  0.034363 
+dt = 6*10**-7
+
+# voor een ideale accelerometer dt is hier niet exact 
+# sinds het wel veranderd bij verschillende tijdsmomenten al is die verandering zo goed als insignificant
+veerconstante = dempingsfactor/dt 
+
+# berekend voor beide datasets de response in een ideale accelerometer
+response_acceleration_data_1_met_ideale_accelerometer = DataProcessor.get_full_response_data(
+        acceleratie_frame = acceleratie_data_frame_1, 
+        timestamps_frame = timestamps_data_1, 
+        response_bij_t_0 = response_bij_t_0, 
+        response_bij_t_0_plus_dt = response_bij_t_0_plus_dt, 
+        dempingsfactor = dempingsfactor, 
+        massa = massa, 
+        veerconstante = veerconstante
+        ) 
+response_acceleration_data_2_met_ideale_accelerometer = DataProcessor.get_full_response_data(
+        acceleratie_frame = acceleratie_data_frame_2, 
+        timestamps_frame = timestamps_data_2, 
+        response_bij_t_0 = response_bij_t_0, 
+        response_bij_t_0_plus_dt = response_bij_t_0_plus_dt, 
+        dempingsfactor = dempingsfactor, 
+        massa = massa, 
+        veerconstante = veerconstante
+        ) 
+
+max_data_index = len(timestamps_data_1)
+
+#deze functie plot de acceleratie data van het frame voor beide databestanden
+DataProcessor.plot_Data(
+    timestamps_data_1, 
+    "Versnellingsprofielen van 2 datasets",
+    "tijd[s]",
+    "versnelling[m*s^-2]",
+    acceleratie_data_frame_1[:max_data_index], "acceleration_frame_dataset_1", 
+    acceleratie_data_frame_2[:max_data_index], "acceleration_frame_dataset_2", 
+    )
+
+#deze functie plot de response voor beide acceleratie profielen binnen een ideale accelerometer
+DataProcessor.plot_Data(
+    timestamps_data_1, 
+    "ideale response van 2 datasets",
+    "tijd[s]",
+    "versnelling[m*s^-2]",
+    response_acceleration_data_1_met_ideale_accelerometer[:max_data_index], "response_massa_dataset_1", 
+    response_acceleration_data_2_met_ideale_accelerometer[:max_data_index], "response_massa_dataset_2", 
+    )
+
+# gegeven veerconstantes
+veerconstante = 32
+
+# berekend voor beide datasets de response in de accelerometer met de gegeven parameters
+response_acceleration_data_1 = DataProcessor.get_full_response_data(
+        acceleratie_frame = acceleratie_data_frame_1, 
+        timestamps_frame = timestamps_data_1, 
+        response_bij_t_0 = response_bij_t_0, 
+        response_bij_t_0_plus_dt = response_bij_t_0_plus_dt, 
+        dempingsfactor = dempingsfactor, 
+        massa = massa, 
+        veerconstante = veerconstante
+        ) 
+response_acceleration_data_2 = DataProcessor.get_full_response_data(
+        acceleratie_frame = acceleratie_data_frame_2, 
+        timestamps_frame = timestamps_data_2, 
+        response_bij_t_0 = response_bij_t_0, 
+        response_bij_t_0_plus_dt = response_bij_t_0_plus_dt, 
+        dempingsfactor = dempingsfactor, 
+        massa = massa, 
+        veerconstante = veerconstante
+        ) 
+
+DataProcessor.plot_Data(
+    timestamps_data_1, 
+    "response profielen van 2 datasets",
+    "tijd[s]",
+    "afstand[m]",
+    response_acceleration_data_1[:max_data_index], "response_massa_dataset_1", 
+    response_acceleration_data_2[:max_data_index], "response_massa_dataset_2"
+    )
