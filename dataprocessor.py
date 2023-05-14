d9e3ac3d16edf306d1face900d2d9ee448667eee
Tim <targa2002260@gmail.com>
2023-05-14 19:40:36 +0200

    updated final reqs and restructuered file system to be better

--- dataprocessor.py
+++ dataprocessor.py
@@ -1,125 +1,243 @@
-
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Apr 21 14:05:27 2023
-
-@author: kayleepont
-"""
-# hier worden de gebruikte libraries geimporteerd. 
-import csv
 import matplotlib.pyplot as plt
 import numpy as np
 
-# [Tim] ik zou het stukje created on bla bla bla lekker weglaten. 
-#       verder is het niet zozeer nodig om te benoemen waar wordt geimporteerd dat is wel duidelijk
-#       in de bijna alle gevallen is het ook handig om geen code of comments boven je imports te plaatsen.
-#       niet zozeer dat dat echt heel erg is maar dat is wel de algemene conventie om overzicht en structuur te behouden
+class DataProcessor():
+    def get_full_response_data(
+        acceleratie_frame: list, 
+        timestamps_frame: list, 
+        response_bij_t_0: float, 
+        response_bij_t_0_plus_dt: float, 
+        dempingsfactor: float, 
+        massa: float, 
+        veerconstante: float
+        ):
+        """
+        Get_full_response_data
+        ----------
+        Deze functie gebruikt de parameters van de accelerometer
+        en het acceleratie profiel van het frame om numeriek
+        de response van de massa te bepalen.
 
-#       de import van csv wordt niet gebruikt dus die kan je weghalen.
+        Parameters
+        ----------
+        - acceleratie_frame : list, 
+            de versnelling van het frame
+        - timestamps_frame : list, 
+            Tijdsmomenten van data.
+        - dempingsfactor : float, 
+            de dempingsfactor gamma is de denpingsfactor van het massaveersysteem.
+        - response_bij_t_0 : float,
+            de response op het startmoment van het versnellingsprofiel.
+        - response_bij_t_0_plus_dt: float, 
+            De response een tijdsmoment dt na de start van het versnellingsprofiel.
+        - massa : float, 
+            De de massa van de massa
+        - veerconstante : float, 
+            het collectieve veerconstante van alle veren in het massa veersysteem
+        
+        Returns
+        ----------
+        response_acceleration_data : list
+            -   een reponse profiel op het gegeven acceleratie profiel in de vorm van een lijst.
+        """
+        
+        input_data_indexes = range(1, len(acceleratie_frame))
+        response_acceleration_data = [response_bij_t_0, response_bij_t_0_plus_dt]
+        
+        for input_data_index in input_data_indexes:
+            response_at_t = DataProcessor.response_T_Plus_dt(
+                dempingsfactor = dempingsfactor,
+                massa = massa,
+                veerconstante = veerconstante,
+                acceleratie_frame_t = acceleratie_frame[input_data_index],
+                response_t = response_acceleration_data[input_data_index],
+                response_t_min_dt = response_acceleration_data[input_data_index - 1],
+                dt = timestamps_frame[input_data_index] - timestamps_frame[input_data_index - 1]
+                )
+            response_acceleration_data.append(response_at_t)
+        return response_acceleration_data
+        
+    def response_T_Plus_dt(
+        dempingsfactor: float, 
+        massa: float, 
+        veerconstante: float, 
+        acceleratie_frame_t: float, 
+        response_t: float, 
+        response_t_min_dt: float, 
+        dt: float
+        ):
+        """
+        Response_T_Plus_dt
+        ----------
+        Berekend de response in een gegeven punt
+        
+        Parameters
+        ----------
+        - dempingsfactor: float, 
+            de dempingsfactor gamma is de denpingsfactor van het massaveersysteem.
+        - massa: float, 
+            De de massa van de massa
+        - veerconstante: float, 
+            het collectieve veerconstante van alle veren in het massa veersysteem
+        - acceleratie_frame_t: float, 
+            de versnelling van het frame op een gegeven tijd t
+        - response_t: 
+            float, de response of relatieve positie van de massa ten opzichten van het frame op een gegeven tijd t
+        - response_t_min_dt: float, 
+            de response of relatieve positie van de massa ten opzichten van het frame op een gegeven tijd t - h
+        - dt: float, 
+            Het verschil in tijdsmomenten.
+            
+        Returns
+        ----------
+        - response_t_plus_dt : float
+            de response van een gegeven punt
+        
+        """
+        response_t_plus_dt = (
+                              response_t            * ( 2 - (dempingsfactor*dt/massa) - (dt**2)*veerconstante/massa )
+                            - response_t_min_dt     * ( 1 - (dempingsfactor*dt/massa) ) 
+                            + acceleratie_frame_t   * ( (dt**2) )
+                            )
+        return response_t_plus_dt
 
-# deze lijn zorgt ervoor dat de data file wordt gelezen. 
-file = 'posities_1_Team_A2.txt'
-data = np.genfromtxt(file,delimiter='   ')
+    def load_Data(filepath: str):
+        """
+        Load_Data
+        ----------
+        laad de databestanden in op basis van een gegeven pad
+        
+        Parameters
+        ----------
+        - filepath: str
+            het pad van het bestand dat je wil inlezen
+        
+        Returns
+        ----------
+        - data: np.array
+            de data van het bestand in de vorm van een numpy array
+        """
+        
+        data = np.genfromtxt(filepath,delimiter='   ')
+        return data
 
-# [Tim] de naam file is niet helemaal correct hier
-#       omdat je hier eigenlijk alleen het pad van de file opslaat 
-#       de naam file_path zou hier beter passen
-
-
-# deze twee regels geven een concrete definitie voor x en t wat het dus makkelijker maakt om versnellingsprofielen te maken. 
-t = data[:,0]
-x = data[:,1] 
-
-# [Tim] t en x zou kunnen voor enkele waardes maar omdat je hier een lijsten aan waardes hebt
-#       is een naam zoals t_values, x_values wat beter. 
-#       als je deze waardes in een loop draait is het ook beter om deze x_value, t_value te noemen.
-#       een loop zou er dan uit zien als:
-#   	"for x_value in x_values" dit leest eigenlijk al als een zin en is dus veel leespaarder dan de namen x, t
-
-# hier wordt er een lege lijst gemaakt voor v, vervolgens wordt x (positie) gedifferentieerd om v (snelheid) te krijgen. 
-v = [] 
-for i in range(0,len(t)-1):
-    dx= x[i+1]-x[i]
-    dt= t[i+1]-t[i]
-    afgeleide = dx/dt
-    v.append(afgeleide) 
-
-# [Tim] de naam i hier is opzich prima omdat het maar gaat over een klein stuk code.
-#       echter is het verstandig om een naam aan te houden die wat beter omschrijft
-#       wat i is een betere naam zou bijvoorbeeld 'data_point_index' kunnen zijn.
-#       de code leest dan ook wat fijner. dan leest het namelijk als:
-#       'for data_point_index in range(0,len(t)-1)'
-
-#       verder kan je ook range(0,len(t)-1) een variabele maken voor je het in een loop doet
-#       die kun je dan bijvoorbeeld 'data_point_indexes' noemen dan krijg je:
-#       'for data_point_index in data_point_indexes'
-#   	wat al helemaal fijn leest.
-
-#       mijn eerdere punt over namen van lijsten en meervoud is ook van toepassing op v
-#       de namen v_values, en v_value zou dan dus ook beter werken
-
-
-# Deze lijnen zijn bijna iedentiek aan degene hierboven, behalve wordt v (snelheid) gedifferentieerd om a (versnelling) te krijgen. 
-a = []
-
-for i in range(0,len(t)-2):
-    dv= v[i+1]-v[i]
-    dt= t[i+1]-t[i]
-    dubbeleafgeleide = dv/dt
-    a.append(dubbeleafgeleide) 
-
-# [Tim] mijn eerdere punten over i en lijstnaamgeving geld hier ook.
-
-# deze lijn zorgt ervoor dat de tweede data file wordt gelezen. 
-file_twee = 'posities_2_Team_A2.txt'
-data_twee = np.genfromtxt(file_twee,delimiter='   ')
-
-# deze twee regels geven een concrete definitie voor x en t wat het dus makkelijker maakt om versnellingsprofielen te maken. 
-t_twee = data_twee[:,0]
-x_twee = data_twee[:,1] 
-
-# hier wordt er een lege lijst gemaakt voor v, vervolgens wordt x (positie) gedifferentieerd om v (snelheid) te krijgen. 
-v_twee = [] 
-for h in range(0,len(t)-1):
-    dx_twee= x_twee[h+1]-x_twee[h]
-    dt_twee= t_twee[h+1]-t_twee[h]
-    afgeleide_twee = dx_twee/dt_twee
-    v_twee.append(afgeleide_twee) 
-
-
-# Deze lijnen zijn bijna iedentiek aan degene hierboven, behalve wordt v (snelheid) gedifferentieerd om a (versnelling) te krijgen. 
-a_twee = [] 
-for h in range(0,len(t)-2):
-    dv_twee= v_twee[h+1]-v_twee[h]
-    dt_twee= t_twee[h+1]-t_twee[h]
-    dubbeleafgeleide_twee = dv_twee/dt_twee
-    a_twee.append(dubbeleafgeleide_twee) 
-
-# [Tim] voor de code die het tweede datafile ophaalt gelden alle comments over de eerste ook.
-#       hierbij wil ik wel nog meegeven dat hier in plaats van i h gebruikt wordt.
-#       dit kan toevoegen aan de onduidelijkheid van i gebruiken sinds je essentieel hetzelfde doet
-#       maar het heeft opeens een andere naam.
-
-#       verder is het mischien wat compacter om in plaats van a_twee de naam a_2 te gebruiken.
-#       de informatie in beide namen is hetzelfde maar a_2 neemt wat minder ruimte in beslag
-#       wat fijn kan zijn om te voorkomen dat je regels niet heel lang worden.
-
-# het maken van een array voor beide versnellingsprofielen. de t staat zelf al wel in een array dus die hoefde niet veranderd te worden. 
-a_array= np.array(a)
-a_twee_array = np.array(a_twee)
-
-# beide datasets hebben nu een versnellingsprofiel die in een array staan. Deze kunnen nu samen in een grafiek gezet worden. 
-plt.plot(t[:-2],a_array, label= 'versnelling_dataset1' )
-plt.plot(t[:-2],a_twee_array, label= 'versnelling_dataset2')
-plt.xlabel('tijd[s]')
-plt.ylabel('Versnelling[m/sˆ2]')
-plt.title("Versnellingsprofielen van twee datasets")
-plt.legend()
-plt.show()
-
-# [Tim] voor de rest is de code over het algemeen heel netjes complimenten!
-#       ik weet dat ik nu veel aandachtspunten geef maar in het algemeen is het oprecht hele nette code
-#       alle comments die ik nu geef zijn echt puntjes op de i dingen. die vooral handig zijn zodat iedereen
-#       de code goed kan lezen onafhankelijk van voorkennis van deze code. 
-#       Ook zodat jij de code in de toekomst direct en makkelijk kan begrijpen
+    def differentiatie(x: list, t: list):
+        """
+        Differentiatie
+        --------------
+        hier wordt er een lege lijst gemaakt voor v, vervolgens wordt x (positie) gedifferentieerd om v (snelheid) te krijgen. 
+        
+        Parameters
+        ----------
+        - x: list
+            functie waardes waar over wordt gediferentieerd
+        - t: list
+            
+        """
+        v = [] 
+        for i in range(0,len(t)-1):
+            dx= x[i+1]-x[i]
+            dt= t[i+1]-t[i]
+            afgeleide = dx/dt
+            v.append(afgeleide)
+        return v, t[:-1]
+    
+    def plot_2_graphs_with_2_different_scales(
+        time_data: list, 
+        plot_title: str, 
+        x_axis_title: str, 
+        dataset_1: list, 
+        graph_1_title: str, 
+        graph_1_y_label: str,
+        dataset_2: list, 
+        graph_2_title:str,
+        graph_2_y_label: str,
+        max_data_index: int 
+        ):
+        """
+        plot_2_graphs_with_2_different_scales
+        ---------
+        plot 2 grafieken op basis van gegeven x data
+        
+        Parameters
+        ----------
+            - time_data: list, 
+                x data van grafiek
+            - plot_title: str, 
+                titel van de plot
+            - x_axis_title: str, 
+                x as titel
+            - dataset_1: list, 
+                y data van eerste dataset
+            - graph_1_title: str, 
+                titel van eerste dataset
+            - graph_1_y_label: str,
+                y label of first graph
+            - dataset_2: list, 
+                y data van tweede dataset
+            - graph_2_title:str
+                titel van tweede dataset
+            - graph_1_y_label: str,
+                y label of first graph
+            - max_data_index: int 
+                maximale index van data
+        """
+        dataset_1_array= np.array(dataset_1)
+        dataset_2_array = np.array(dataset_2)
+        fig, axis_1 = plt.subplots()
+        axis_1.plot(
+            time_data[:max_data_index], 
+            dataset_1_array[:max_data_index],
+            label =  graph_1_title, 
+            color = "green"
+            )
+        axis_2 = axis_1.twinx()
+        axis_2.plot(
+            time_data[:max_data_index], 
+            dataset_2_array[:max_data_index], 
+            label = graph_2_title, 
+            color = "red"
+            )
+        axis_1.set_xlabel(x_axis_title)
+        axis_1.set_ylabel(graph_1_y_label)
+        axis_2.set_ylabel(graph_2_y_label)
+        axis_1.set_title(plot_title)
+        axis_1.legend(loc=2)
+        axis_2.legend(loc=0)
+        plt.show()
+        
+    def plot_Data_scalable(
+        t: list, 
+        plot_title: str, 
+        x_axis_title: str, 
+        y_axis_title: str, 
+        datasets: list,
+        max_data_index: int 
+        ):
+        """
+        plot_Data_scalable
+        ---------
+        plot n grafieken op basis van gegeven data
+        
+        Parameters
+        -----------
+            - t: list,
+                x data van grafiek
+            - plot_title: str, 
+                naam van plot
+            - x_axis_title: str, 
+                x as titel
+            - y_axis_title: str, 
+                y as titel
+            - datasets: list,
+                alle datasets met grafiek titels
+            - max_data_index: int 
+                maximale index van data
+        """
+        for dataset in datasets:
+            plt.plot(t[:max_data_index],dataset["x_data"][:max_data_index], label=  dataset["title"])
+        plt.xlabel(x_axis_title)
+        plt.ylabel(y_axis_title)
+        plt.title(plot_title)
+        plt.legend()
+        plt.show()
