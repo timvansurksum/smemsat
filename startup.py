8dfede8a7068915760b8d294fa6eb1938fc9756c
leekaycodes <131393192+leekaycodes@users.noreply.github.com>
2023-06-13 14:47:46 +0200

    Add files via upload

--- startup.py
+++ startup.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 # Gekregen parameters voor stap 1.
 V_0                                     =  15 #V
@@ -29,7 +29,7 @@ F_electric           = lambda t: 1/2 * v(t)**2 * c_derivative(t) * CAPACITOR_PLA
 transfer_coefficient = F_electric(0.1)/v(0.1) # 
 
 def process_data_drive (DAMPING_COEFICIENT, SPRING_RATE_DRIVE, MASS_OF_DRIVE, F_electric):
-    Nstap    = 1000+1
+    Nstap    = 100000+1
     teind    = 0.5 
     dt       = teind/(Nstap -1)       
     t_values = np.linspace(0, teind, Nstap)
@@ -48,7 +48,7 @@ def process_data_drive (DAMPING_COEFICIENT, SPRING_RATE_DRIVE, MASS_OF_DRIVE, F_
     
     # numerieke oplossing van de differentiaal vergelijking.
     for time_index in range(1, Nstap - 1):
-         x_values[time_index+1]  =  component_a * x_values[time_index] +  component_b * x_values[time_index-1] + component_c * F_electric(t_values)[time_index] 
+         x_values[time_index+1]  =  component_a * x_values[time_index] +  component_b * x_values[time_index-1] + component_c * F_electric(t_values[time_index]) 
                         
     # amplitude berekenen
     amplitude_x_drive   = np.max(x_values)
@@ -57,6 +57,7 @@ def process_data_drive (DAMPING_COEFICIENT, SPRING_RATE_DRIVE, MASS_OF_DRIVE, F_
 
 # oproepen van waardes uit functie
 amplitude_x_drive, x_values, t_values = process_data_drive(DAMPING_COEFICIENT, SPRING_RATE_DRIVE, MASS_OF_DRIVE, F_electric)
+
 
 # berekenen van snelheid vanuit x waardes. 
 def compute_velocity (x_values,t_values):
@@ -73,68 +74,27 @@ def compute_velocity (x_values,t_values):
 v_values = compute_velocity(x_values, t_values)
 
 # gekregen parameters voor stap 3.
-ANGULAR_VELOCITY             = 10 #rad/min
+ANGULAR_VELOCITY             = 10/60 #rad/sec
 
-# hoeksnelheid in een array zetten.
-angular_velocity_values      = np.zeros_like(t_values)
-angular_velocity_values[0]   = ANGULAR_VELOCITY
 
 # F coriolis kracht berekenen. 
-def compute_coriolis_force (MASS_OF_DRIVE, angular_velocity_values, v_values):
+def compute_coriolis_force (MASS_OF_DRIVE, ANGULAR_VELOCITY, v_values):
     F_coriolis  = []
     for time_index in range(0,len(t_values)-1):
-        force = - 2 * MASS_OF_DRIVE * angular_velocity_values[time_index] * v_values[time_index]
+        force = - 2 * MASS_OF_DRIVE * ANGULAR_VELOCITY * v_values[time_index]
         F_coriolis.append(force)
+    F_coriolis.append(F_coriolis[-1])
+    print(force)
     return(F_coriolis)
 
-F_coriolis  = compute_coriolis_force(MASS_OF_DRIVE, angular_velocity_values, v_values)
+F_coriolis  = compute_coriolis_force(MASS_OF_DRIVE, ANGULAR_VELOCITY, v_values)
 
+plt.plot(t_values,F_coriolis)
 
 # Stap 4
 MASS_OF_SENSE           = 4.92e-9 #kilogram
 SPRING_RATE_SENSE       = 0.7769 #N/m
 ANGULAR_VELOCITY_SENSE  = 1.237e-06 # kg/s
-
-def verwerk_data_sense (ANGULAR_VELOCITY_SENSE, SPRING_RATE_SENSE): 
-    Nstap    = 100000+1
-    teind    = 0.5 
-    dt       = teind/(Nstap -1)       
-    t_values = np.linspace(0, teind, Nstap)
-
-    # x en v berekeningen voor de differentiaal vergelijking.
-    x0          = 0      # begin positie
-    v0          = 0      # begin snelheid
-    x_values    = np.zeros_like(t_values)
-    x_values[0] = x0
-    x_values[1] = x0 + v0 * dt
-    
-    # omega array
-    omega_values = np.zeros_like(t_values)
-    for tijd_index in range(20000,80000):
-        omega_values[tijd_index] = omega
-        
-    # differentiaal vergelijking constantes.
-    a = - (veerconstante - 2 * massa / (dt**2)) / (massa /(dt**2)+gamma/(2*dt))
-    b = - ( massa /( dt ** 2) - gamma / ( 2 * dt)) / ( massa /( dt**2) + gamma /(2*dt))
-    c = f_coriolis_geschaald / (massa/ (dt**2) + gamma / (2*dt))
-    
-    # numerieke oplossing van de differentiaal vergelijking.
-    for tijd_index in range(1, Nstap - 1):
-        x_values[tijd_index+1]  = a * x_values[tijd_index] + b * x_values[tijd_index-1]+ c * omega_values[tijd_index] * np.cos(drive_omega * t_values[tijd_index])   
-         
-    amplitude_x_sense   = np.max(x_values)
-    
-    # respons tijd berekenen door middel van maximale amplitude en t waardes. 
-    elementen       = np.where(x_values>= amplitude_x_sense - amplitude_x_sense/math.e)
-    element         = elementen[0][0]
-    t0              = t_values[20000] # dit geeft aan vanaf welk punt er gemeten moet worden]. 
-    t1              = t_values[element]
-    respons_tijd    = t1 - t0 
-    
-    statische_uitwijking = f_coriolis_geschaald / veerconstante
-    Q_factor = (amplitude_x_sense / statische_uitwijking)
-    
-    return(amplitude_x_sense, Q_factor, respons_tijd, x_values, t_values, omega_values)
 
 
 # Stap 5
