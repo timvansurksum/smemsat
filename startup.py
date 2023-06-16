431cdca8aabe29e40d4bdd624aa56a7bce821e5b
Tim <targa2002260@gmail.com>
2023-06-16 22:24:33 +0200

    added transfer coeficients

--- startup.py
+++ startup.py
@@ -30,12 +30,14 @@ Nstap    = 100000+1
 teind    = 0.5 
 t_values = np.linspace(0, teind, Nstap)
 F_electric_values = list(map(F_electric, t_values))
+v_values = list(map(v, t_values))
 plt.figure(1)
 plt.plot(t_values, F_electric_values, color='pink')
 plt.title('F_electric')
 
 # berekenen van transmissiecoefficient n1.
-transfer_coefficient_step1 = F_electric(0.1)/v(0.1) 
+transfer_coefficient_step_1 = max(F_electric_values)/max(v_values)
+print(f'transfer coeficient of f_electric to velocity {transfer_coefficient_step_1}')
 
 def process_data_drive (DAMPING_COEFICIENT_DRIVE, SPRING_RATE_DRIVE, MASS_OF_DRIVE, F_electric_values):
     Nstap    = 100000+1
@@ -67,6 +69,9 @@ def process_data_drive (DAMPING_COEFICIENT_DRIVE, SPRING_RATE_DRIVE, MASS_OF_DRI
 # oproepen van waardes uit functie
 amplitude_x_drive, x_values_drive, t_values = process_data_drive(DAMPING_COEFICIENT_DRIVE, SPRING_RATE_DRIVE, MASS_OF_DRIVE, F_electric_values)
 
+transfer_coefficient_step_2 = max(v_values)/amplitude_x_drive
+print(f'transfer coeficient of v to drive amplitude {transfer_coefficient_step_2}')
+
 plt.figure(2)
 plt.plot(t_values, x_values_drive, color='pink')
 plt.title('x_values_drive')
@@ -86,10 +91,6 @@ def compute_velocity (x_values_drive,t_values):
 # oproepen van waardes uit functie
 v_values_drive = compute_velocity(x_values_drive, t_values)
 
-# berekenen van de transmissiecoefficient n2
-transfer_coefficient_step2 = amplitude_x_drive/v_values_drive
-
-
 
 # gekregen parameters voor stap 3.
 ANGULAR_VELOCITY = 10/60 #rad/sec
@@ -105,6 +106,9 @@ def compute_coriolis_force (MASS_OF_DRIVE, ANGULAR_VELOCITY, v_values_drive):
     return(F_coriolis)
 
 F_coriolis  = compute_coriolis_force(MASS_OF_DRIVE, ANGULAR_VELOCITY, v_values_drive)
+
+transfer_coefficient_step_3 = amplitude_x_drive/max(F_coriolis)
+print(f'transfer coeficient of amplitude_x_drive to F_coriolis {transfer_coefficient_step_3}')
 
 plt.figure(3)
 plt.plot(t_values, F_coriolis, color='pink')
@@ -145,6 +149,10 @@ def process_data_sense (DAMPING_COEFICIENT_SENSE, SPRING_RATE_SENSE, MASS_OF_SEN
     return(amplitude_x_sense, x_values_sense, t_values)
 
 amplitude_x_sense, x_values_sense, t_values = process_data_sense(DAMPING_COEFICIENT_SENSE, SPRING_RATE_SENSE, MASS_OF_SENSE, F_coriolis)
+
+transfer_coefficient_step_4 = max(F_coriolis)/max(x_values_sense)
+print(f'transfer coeficient of F_coriolis to x_values_sense {transfer_coefficient_step_4}')
+
 plt.figure(4)
 plt.plot(t_values, x_values_sense, color='pink')
 plt.title('x_values_sense')
@@ -162,6 +170,10 @@ I_extra_factor = SENSE_CAPACITOR_COUNT*PERMMITIVITY_OF_FREE_SPACE*W_SENSE/D_SENS
 i_sense = lambda t_index: I_extra_factor * v_values_sense[int(t_index)]
 t_indexes = range(0, len(t_values))
 i_sense_values = list(map(i_sense, t_indexes))
+
+transfer_coefficient_step_5 = max(x_values_sense)/max(i_sense_values)
+print(f'transfer coeficient of x_sense_values to i_sense_values {transfer_coefficient_step_5}')
+print(f'transfer coeficient of f_electric to i_sense_values or the whole system {transfer_coefficient_step_1*transfer_coefficient_step_2*transfer_coefficient_step_3*transfer_coefficient_step_4*transfer_coefficient_step_5}')
 
 plt.figure(5)
 plt.plot(t_values, i_sense_values, color='pink')
